GIN implemented, softmax fixed, curve looking okay

src/replication/mutag.py
@@ -8,27 +8,27 @@ import torch
 from dig.xgraph.dataset import MoleculeDataset
 from matplotlib import pyplot as plt
 from torch import optim
-from torch.nn import ReLU, Linear, Softmax, Sigmoid
+from torch.nn import ReLU, Linear, Softmax, Sigmoid, Dropout
 from torch_geometric.data import Data
 from torch_geometric.loader import DataLoader
-from torch_geometric.nn import Sequential, GCNConv, global_max_pool, GNNExplainer
+from torch_geometric.nn import Sequential, GCNConv, GINConv, global_max_pool, GNNExplainer
 from torch_geometric.utils import to_networkx
 
 from src.algorithm.subgraph_x import SubgraphX
-from src.utils.logging import load_data, save_data
+from src.utils.logging import load_data, save_data, aggregate_fidelity_sparsity, compute_avg_runtime, save_str
 from src.utils.metrics import sparsity, fidelity
 from src.utils.task_enum import Task
 from src.utils.training import train_model, test
 from src.utils.utils import get_device, set_seed, get_predicted_class, convert_edge_mask_to_subset
+from src.utils.visualization import plot_results
 
 
 def download_and_prepare_dataset() -> MoleculeDataset:
     return MoleculeDataset('./datasets', 'MUTAG')
 
 
-def split_dataset(dataset: MoleculeDataset, batch_size: int = 32, split_ratio = (0.8,0.9)) \
+def split_dataset(dataset: MoleculeDataset, batch_size: int = 32, split_ratio=(0.8, 0.9)) \
         -> Tuple[DataLoader, DataLoader, DataLoader, List[Data]]:
-
     collated_graph = dataset.data
     num_graphs = len(collated_graph.y)
 
@@ -81,7 +81,7 @@ def split_dataset(dataset: MoleculeDataset, batch_size: int = 32, split_ratio = 
     return train_loader, dev_loader, test_loader, dev_list
 
 
-def get_model():
+def get_model_gcn():
     device = get_device()
 
     input_dim = 7
@@ -106,10 +106,37 @@ def get_model():
     return model
 
 
-def train_model_or_load(train_loader, dev_loader):
-    save_dst = './checkpoints/mutag/gcn.pt'
-    model = get_model()
+def get_model_gin():
+    device = get_device()
+
+    input_dim = 7
+    hidden_dim = 128
+    output_dim = 2
+
+    model = Sequential(
+        'x, edge_index, batch', [
+            (GINConv(torch.nn.Sequential(Linear(input_dim, hidden_dim), ReLU(inplace=True),
+                                         Linear(hidden_dim, hidden_dim), ReLU(inplace=True))), 'x, edge_index -> x'),
+            (GINConv(torch.nn.Sequential(Linear(hidden_dim, hidden_dim), ReLU(inplace=True),
+                                         Linear(hidden_dim, hidden_dim), ReLU(inplace=True))), 'x, edge_index -> x'),
+            (GINConv(torch.nn.Sequential(Linear(hidden_dim, hidden_dim), ReLU(inplace=True),
+                                         Linear(hidden_dim, hidden_dim), ReLU(inplace=True))), 'x, edge_index -> x'),
+            (global_max_pool, 'x, batch -> x'),
+            Linear(hidden_dim, hidden_dim),
+            ReLU(),
+            Dropout(),
+            Linear(hidden_dim, output_dim)
+        ]
+    ).to(device)
+
+    return model
+
+
+def train_model_or_load(train_loader, dev_loader, model_type='gcn', add_softmax=True):
+    save_dst = f'./checkpoints/mutag/{model_type}.pt'
+    model = get_model_gcn() if model_type == 'gcn' else get_model_gin()
     loss_func = torch.nn.CrossEntropyLoss()
+    device = get_device()
 
     if os.path.isfile(save_dst):  # if checkpoint exists, load it
         model.load_state_dict(torch.load(save_dst))
@@ -118,8 +145,17 @@ def train_model_or_load(train_loader, dev_loader):
         num_epochs = 2000
         output_freq = 100
         train_model(model, False, optimizer, train_loader, dev_loader, num_epochs, loss_func, save_dst, output_freq)
-
-    return model, loss_func
+    if add_softmax:
+        # cross entropy loss already contains softmax, therefore just add softmax layer after training
+        result_model = Sequential(
+            'x, edge_index, batch', [
+                (model, 'x, edge_index, batch -> x'),
+                Softmax(dim=1),
+            ]
+        ).to(device)
+        return result_model, loss_func
+    else:
+        return model, loss_func
 
 
 def collect_subgraphx_expl(model, graph_list):
@@ -140,12 +176,10 @@ def collect_subgraphx_expl(model, graph_list):
     # collect explanations for all nodes with a fixed n_min
     subgraphx = SubgraphX(model, num_layers=3, exp_weight=10, m=20, t=100)
 
-
     for n_min in [4]:
         print(f'\nstarting n_min={n_min}')
         counter = 1
         for g in test_graph_idx:
-
             graph = graph_list[g]
             print(graph.x.shape)
             start_time = time.time()
@@ -166,20 +200,19 @@ def collect_subgraphx_expl(model, graph_list):
     return res_dict
 
 
-def collect_gnn_expl(model, graph_list):
+def collect_gnn_expl(model, graph_list, path='./result_data/mutag/gcn_gnnexp'):
     device = get_device()
 
     num_graphs = len(graph_list)
     test_graph_idx = torch.arange(0, num_graphs).tolist()
 
-    path = './result_data/mutag/gcn_gnnexp'
     if os.path.isfile(path):
         res_dict = load_data(path)
     else:
         res_dict = dict()
         for g in test_graph_idx:
             res_dict[g] = []
-        # save_data(path, res_dict)
+        save_data(path, res_dict)
 
     # collect explanations for all nodes with a fixed n_min
     explainer = GNNExplainer(model, epochs=2000, return_type='prob')
@@ -201,6 +234,7 @@ def collect_gnn_expl(model, graph_list):
             fidelity_score = fidelity(graph, explanation_set, model)
 
             result_tuple = (explanation_set, sparsity_score, fidelity_score, duration)
+            #print(result_tuple)
             res_dict[g] = res_dict[g] + [result_tuple]
 
         print(f'\nfinished {counter} of {num_graphs}')
@@ -214,16 +248,33 @@ def main():
     set_seed(0)  # IMPORTANT!
     dataset = download_and_prepare_dataset()
 
-    batch_size = 100
+    batch_size = 188
     train_loader, dev_loader, test_loader, dev_list = split_dataset(dataset, batch_size, (0.8, 1.0))
 
-    model, loss_func = train_model_or_load(train_loader, dev_loader)
+    model_type = 'gin'
 
+    model, loss_func = train_model_or_load(train_loader, dev_loader, model_type, add_softmax=True)
+    print(model)
     test_loss, test_acc = test(model, False, dev_loader, loss_func)
     print(f'test loss: {test_loss}, test_acc: {test_acc}')
 
-    collect_gnn_expl(model, dev_list)
-    print('done')
+    path = f'./result_data/mutag/{model_type}_gnnexp'
+    collect_gnn_expl(model, dev_list, path=path)
+
+    gnn_dict = load_data(path)
+    gnn_sparsity, gnn_fidelity = aggregate_fidelity_sparsity(gnn_dict)
+    gnn_runtime = compute_avg_runtime(gnn_dict)
+
+    # plot graph
+    sparsity_list = [gnn_sparsity]
+    fidelity_list = [gnn_fidelity]
+    labels = ['GNN Explainer']
+    plot_results(sparsity_list, fidelity_list, labels, save_dst=f'./img/mutag/{model_type}_result.png')
+
+    # save runtime to file
+    data_str = f'GNN Explainer: {gnn_runtime}'
+    save_str(path=f'./result_data/mutag/{model_type}_runtime.txt', data=data_str)
+    print(data_str)
 
 
 if __name__ == '__main__':